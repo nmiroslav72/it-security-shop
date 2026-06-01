34d8d0ea85f91523cdeac8a78e9db5260412ef5d
Miroslav Nikolic <nmiroslav72@yahoo.com>
2026-06-02 00:42:06 +0200

    Fix suspense bailout

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,6 @@ const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
+  experimental: { missingSuspenseWithCSRBailout: false },
 };
 export default nextConfig;
