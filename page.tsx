49922fd28d0b1d1fbaaebfd425d8ba84b800e9da
Miroslav Nikolic <nmiroslav72@yahoo.com>
2026-06-02 00:11:12 +0200

    Remove old scripts

--- page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-// Početna stranica vodi direktno na prodavnicu
-// Ako želiš posebnu početnu stranicu, zameni redirect sa pravim sadržajem
-export default function HomePage() {
-  redirect("/shop");
-}
