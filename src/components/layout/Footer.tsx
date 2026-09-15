94412a21bf22a5fc63b5c5d688bde2a4a8234046
nmiroslav72 <nmiroslav72@yahoo.com>
2026-09-15 07:25:03 +0200

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -74,7 +74,7 @@ export function Footer() {
         <div className="site-footer__col">
           <h4 className="site-footer__col-title">Kontakt</h4>
           <ul className="site-footer__contact">
-                       <li><a href="tel:+381632246510">063 224 651</a></li>
+                               <li><a href="tel:+38163224651">063 224 651</a></li>
             <li><a href="mailto:diskontvideonadzora@diskontvideonadzora.rs">diskontvideonadzora@diskontvideonadzora.rs</a></li>
             <li>109. nova 18, 11060 Beograd (Palilula)</li>
             <li className="site-footer__hours">Pon-Pet: 09-17h</li>
