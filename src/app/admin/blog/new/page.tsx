ba76b13b4aea68a47ea9573881b66ef169ada0e0
nmiroslav72 <nmiroslav72@yahoo.com>
2026-09-17 22:13:27 +0200

    Update page.tsx

--- src/app/admin/blog/new/page.tsx
+++ src/app/admin/blog/new/page.tsx
@@ -1,1 +1,7 @@
 
+import PostForm from "../PostForm";
+import { createPost } from "../actions";
+
+export default function NewPost() {
+  return (<div><h1 className="dash-title" style={{ marginBottom:20 }}>Novi tekst</h1><PostForm action={createPost} /></div>);
+}
