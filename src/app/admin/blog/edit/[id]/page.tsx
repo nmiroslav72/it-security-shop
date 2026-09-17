f7fd3af76d7bd05972796b43dd419fb8dc07bfd5
nmiroslav72 <nmiroslav72@yahoo.com>
2026-09-17 22:14:23 +0200

    Update page.tsx

--- src/app/admin/blog/edit/[id]/page.tsx
+++ src/app/admin/blog/edit/[id]/page.tsx
@@ -1,1 +1,17 @@
 
+import { prisma } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import PostForm from "../../PostForm";
+import { updatePost, deletePost } from "../../actions";
+
+export default async function EditPost({ params }) {
+  const { id } = await params;
+  const post = await prisma.post.findUnique({ where: { id } });
+  if (!post) notFound();
+  return (
+    <div>
+      <h1 className="dash-title" style={{ marginBottom:20 }}>Izmena teksta</h1>
+      <PostForm action={updatePost.bind(null, id)} initial={post} onDelete={deletePost.bind(null, id)} />
+    </div>
+  );
+}
